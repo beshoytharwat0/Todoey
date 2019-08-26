28c651e412e40c1bfe20beadfc56a47dc8664c77
mace <beshoy.tharwat0@gmaile.com>
2019-08-26 19:19:30 +0300

    Data persisted by UserDefault method

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class TodoListViewController: UITableViewController  {
     
-    let itemArray = ["Beshyo","Joesph", "Mirna"]
+    var itemArray = ["Beshyo", "Joesph", "yousef",]
+    // 3rft user default
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            
+            itemArray = items
+        }
     }
     
       //MARK - tableview DataSourse Methods
@@ -57,6 +64,37 @@ class TodoListViewController: UITableViewController  {
         // lma b3ml select ll row byfd m3lm 3lih blon el rmady ana 3awz y3ml select lon rmady w y5tfi 3latol
         tableView.deselectRow(at: indexPath, animated: true)
     }
-
+    
+    // MARK : add button items :-
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todo Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //-- what will happen once the usre click add item button on our Alert
+            // ana 3mlt add item mn alertTextField to arrayitem bs mn htzhr 7aga lazm a3ml reload table view
+            self.itemArray.append(textField.text!)
+            
+            self.defaults.setValue(self.itemArray, forKey: "TodoListArray")
+            
+            self.tableView.reloadData()
+            
+            
+        }
+        // how to add textField at alter
+        alert.addTextField { (alertTextFiled) in
+            
+            alertTextFiled.placeholder = "Creat New Item"
+            textField = alertTextFiled
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
 }
 
